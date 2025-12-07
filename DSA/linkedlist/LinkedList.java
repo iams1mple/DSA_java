84d4be71fa5835b708e51af8b07d1be42f43b7a5
iams1mple <ayush.raj021@gmail.com>
2025-12-08 03:57:04 +0530

    linkedList constructor

---
+++ DSA/linkedlist/LinkedList.java
@@ -0,0 +1,41 @@
+package linkedlist;
+
+import java.util.*;
+
+class LinkedList {
+    private Node head;
+    private Node tail;
+    private int length;
+
+    class Node {
+        int value;
+        Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public LinkedList(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+    }
+
+    public void getHead() {
+        System.out.println("Head: " + head.value);
+    }
+    
+    public void getTail() {
+        System.out.println("Tail: " + tail.value);
+    }
+    
+    public void getLength(){
+        System.out.println("Length: " + length);
+    }
+
+    public void printList(){
+        
+    }
+}
